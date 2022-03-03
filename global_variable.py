f7221b9a92297fdcd2ee36b935b13ac09b48f538
flalucifer <57249914+flalucifer@users.noreply.github.com>
2022-03-03 21:19:12 +0800

    Update global_variable.py

--- global_variable.py
+++ global_variable.py
@@ -6,3 +6,4 @@ border = Border(left=Side(border_style='thin', color='000000'), right=Side(borde
                 top=Side(border_style='thin', color='000000'), bottom=Side(border_style='thin', color='000000'))
 alignment = Alignment(horizontal='center', vertical='center', wrapText=True)
 is_exit = False
+
